c76360c2873a8ffaa859348f31fa087495b6314c
Gustavo Nieto <ganv2001@gmail.com>
2023-11-30 13:45:47 +0100

    Added the Header section and the NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,90 @@ import TheCar from "./assets/thecar.png";
 import Tranq from "./assets/tranquility_760x.png";
 import TSL from "./assets/tsrtame.png";
 
+import { useEffect, useState } from "react";
+
 function App() {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleNavToggle = () => {
+    setIsNavVisible((prev) => !prev);
+  };
+
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const toggleCartVisibility = () => {
+    setIsCartVisible(!isCartVisible);
+  };
+
   return (
     <>
+      <header className="header">
+        <nav className={`navbar ${isSticky ? "sticky" : ""}`}>
+          <button
+            onClick={handleNavToggle}
+            aria-controls="primary-navigation"
+            aria-expanded={isNavVisible}
+            className="mobile-nav-toggle"
+          >
+            <span className="menu-button">
+              {isNavVisible ? "Close" : "Menu"}
+            </span>
+          </button>
+          <ul
+            id="primary-navigation"
+            data-visible={isNavVisible}
+            className="primary-navigation"
+          >
+            <li>
+              <a href="./createAccount.html" className="nav-a">
+                create account
+              </a>
+            </li>
+            <li>
+              <a href="./login.html" className="nav-a">
+                login
+              </a>
+            </li>
+            <li>
+              <a href="./products.html" className="nav-a">
+                all products
+              </a>
+            </li>
+            <li>
+              <a href="./about.html" className="nav-a">
+                who we are
+              </a>
+            </li>
+          </ul>
+
+          <button
+            onClick={toggleCartVisibility}
+            aria-controls="cart-btn"
+            aria-expanded={isCartVisible}
+            className="mobile-nav-toggle-cart"
+          >
+            <span className="menu-button">
+              {isCartVisible ? "Close" : "Cart"}
+            </span>
+          </button>
+        </nav>
+        <div className="box">
+          <h1 className="turntable-treasures">TURNTABLE TREASURES</h1>
+        </div>
+      </header>
       <main className="hero">
         <div className="explore-products">
           <h1>Explore our products.</h1>
@@ -45,10 +126,13 @@ function App() {
               <p className="product-title">
                 Tame Impala - The Slow Rush Collectors Edition
               </p>
-              <p className="product-price">$85</p>
-              <a href="#" className="add-to-cart">
-                add to cart
-              </a>
+
+              <div className="price-and-cart">
+                <p className="product-price">$85</p>
+                <a href="#" className="add-to-cart">
+                  add to cart
+                </a>
+              </div>
             </div>
           </div>
           <div className="sm-card">
@@ -61,10 +145,12 @@ function App() {
               <p className="product-title">
                 Arctic Monkeys - Tranquility Base Hotel & Casino
               </p>
-              <p className="product-price">$30</p>
-              <a href="#" className="add-to-cart">
-                add to cart
-              </a>
+              <div className="price-and-cart">
+                <p className="product-price">$30</p>
+                <a href="#" className="add-to-cart">
+                  add to cart
+                </a>
+              </div>
             </div>
           </div>
           <div className="sm-card">
@@ -74,11 +160,13 @@ function App() {
               alt="Arctic Monkeys - The Car"
             />
             <div className="card-body">
-              <h5 className="product-title">Arctic Monkeys - The Car</h5>
-              <p className="product-price">$35</p>
-              <a href="#" className="add-to-cart">
-                add to cart
-              </a>
+              <p className="product-title">Arctic Monkeys - The Car</p>
+              <div className="price-and-cart">
+                <p className="product-price">$35</p>
+                <a href="#" className="add-to-cart">
+                  add to cart
+                </a>
+              </div>
             </div>
           </div>
         </div>
